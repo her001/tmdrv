6d31727bca334e2bfabf21d7ad98be2d2b5761c9
HER0_01 <aconrad103@gmail.com>
2016-02-24 23:43:33 -0600

    Use argparse to add a very basic help text

--- tmdrv.py
+++ tmdrv.py
@@ -77,4 +77,9 @@ def _control_init(idVendor, idProduct, request_type, request, value, index, data
 context = usb1.USBContext()
 
 if __name__ == '__main__':
+	import argparse
+	
+	parser = argparse.ArgumentParser(description=__doc__)
+	args = parser.parse_args()
+	
 	initialize()
