90fec7bbcdfe9a6e7a6f446b63715fb55d3349d0
Andrew Conrad <aconrad103@gmail.com>
2017-05-26 13:18:46 -0500

    Update copyright year

--- tmdrv.py
+++ tmdrv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2016 Andrew Conrad
+# Copyright 2016, 2017 Andrew Conrad
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
