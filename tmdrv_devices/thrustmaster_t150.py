6c0164ed7d0a4fa738910ad8e7083e8dd599b290
Andrew Conrad <aconrad103@gmail.com>
2017-12-11 11:20:26 -0600

    t150: Fix step numbering for control packets

--- tmdrv_devices/thrustmaster_t150.py
+++ tmdrv_devices/thrustmaster_t150.py
@@ -3,7 +3,7 @@ idVendor = 0x044f
 idProduct = [0xb65d, 0xb677, 0xb677]
 control = [
 	{'step':1, 'request_type':0x41, 'request':83, 'value':0x0006, 'index':0x0000, 'data':b''},
-	{'step':1, 'request_type':0x41, 'request':72, 'value':0x0040, 'index':0x0000, 'data':b''},
+	{'step':2, 'request_type':0x41, 'request':72, 'value':0x0040, 'index':0x0000, 'data':b''},
 ]
 jscal = '6,1,0,16439,16439,32879,32879,1,0,511,511,1054724,1052655,1,0,512,512,1052655,1052655,1,0,510,510,1052655,1050595,1,0,0,0,536854528,536854528,1,0,0,0,536854528,536854528'
 dev_by_id = 'usb-Thrustmaster_TRS_Racing_wheel-joystick'
